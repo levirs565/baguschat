79e4567af048052695cd6e00859cd05ac8c5dbe5
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-11-06 07:49:52 +0700

    feat: make typesafe api

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,10 @@
 import { createSRPClient } from "@levirs565/srp";
 import blake from "blakejs";
-import axios, { type AxiosResponse, type AxiosResponseHeaders } from "axios";
+import axios, {
+  type AxiosInstance,
+  type AxiosResponse,
+  type AxiosResponseHeaders,
+} from "axios";
 import { fromHex, toHex } from "@smithy/util-hex-encoding";
 import { fromBase64, toBase64 } from "@smithy/util-base64";
 import {
@@ -21,243 +25,454 @@ import {
   decryptAESGCM,
 } from "./crypto";
 import { getUserPrivateKey, putUserPrivateKey } from "./db";
+import { TypedEventTarget } from "typescript-event-target";
 
 const srpClient = createSRPClient("SHA-256", 2048);
 
-const API_URL = "http://localhost:8080";
-const instance = axios.create({
-  baseURL: API_URL,
-  withCredentials: true,
-});
+interface SignupRequest {
+  username: string;
+  srp_salt: string;
+  srp_verifier: string;
+  private_key_encrypted: string;
+  public_key: string;
+}
 
-async function runRequest(runner: () => Promise<AxiosResponse>) {
-  try {
-    const response = await runner();
+interface SRPHelloRequest {
+  username: string;
+  srp_client_public_key: string;
+}
 
-    if (response.data.error) {
-      throw response.data.error;
+interface SRPHelloResponse {
+  srp_salt: string;
+  srp_server_public_key: string;
+}
+
+interface ActionResultResponse {
+  success: boolean;
+}
+
+interface SRPAuthRequest {
+  srp_evidence: string;
+}
+
+interface SRPAuthResponse {
+  srp_evidence: string;
+}
+
+interface GetStateResponseUser {
+  id: string;
+  username: String;
+}
+
+interface GetStateResponse {
+  user?: GetStateResponseUser;
+}
+
+interface GetKeysResponse {
+  private_key_encrypted: string;
+  public_key: string;
+}
+
+interface GetUserResponse {
+  username: string;
+  public_key: string;
+}
+
+interface ChatItem {
+  id: string;
+  created_at: string;
+  sender_id: string;
+  receiver_id?: string;
+  sender_key: string;
+  receiver_key: string;
+  cipher: string;
+}
+
+class APIService {
+  instance: AxiosInstance;
+  baseUrl: string;
+  constructor(apiUrl: string = "http://localhost:8080") {
+    this.baseUrl = apiUrl;
+    this.instance = axios.create({
+      baseURL: apiUrl,
+      withCredentials: true,
+    });
+  }
+
+  async runRequest<T>(runner: () => Promise<AxiosResponse>): Promise<T> {
+    try {
+      const response = await runner();
+
+      if (response.data.error) {
+        throw response.data.error;
+      }
+
+      return response.data;
+    } catch (e: any) {
+      if (axios.isAxiosError(e)) {
+        if (e.response) {
+          throw e.response.data.error;
+        } else {
+          throw { type: "NetworkError", error: e };
+        }
+      } else {
+        throw { type: "Unknown", error: e };
+      }
+    }
+  }
+
+  get<T>(path: string): Promise<T> {
+    return this.runRequest(() => this.instance.get(path));
+  }
+  post<T>(path: string, data: any): Promise<T> {
+    return this.runRequest(() => this.instance.post(path, data));
+  }
+
+  signup(data: SignupRequest) {
+    return this.post<ActionResultResponse>("/auth/signup", data);
+  }
+
+  srpHello(data: SRPHelloRequest) {
+    return this.post<SRPHelloResponse>("/auth/hello", data);
+  }
+
+  srpAuth(data: SRPAuthRequest) {
+    return this.post<SRPAuthResponse>("/auth/auth", data);
+  }
+
+  logout() {
+    return this.post<ActionResultResponse>("/auth/logout", null);
+  }
+
+  getState() {
+    return this.get<GetStateResponse>("/auth/state");
+  }
+
+  getKeys() {
+    return this.get<GetKeysResponse>("/auth/keys");
+  }
+
+  getUser(id: string) {
+    return this.get<GetUserResponse>(`/user/${id}`);
+  }
+
+  getChats() {
+    return this.get<ChatItem[]>(`/chat`);
+  }
+}
+
+class ClientService {
+  #apiService: APIService;
+
+  constructor(apiService: APIService) {
+    this.#apiService = apiService;
+  }
+
+  async signup(username: string, password: string) {
+    const keyPair = await generateRSAKeyPair();
+    const privateKey = await exportRSAPrivateKey(keyPair.privateKey);
+    const publicKey = await exportRSAPublicKey(keyPair.publicKey);
+
+    const privateKeyEncrypted = await encryptAESGCMPassword(
+      password,
+      privateKey
+    );
+
+    const srpSalt = srpClient.generateSalt();
+    const srpPrivateKey = await srpClient.derivePrivateKey(
+      srpSalt,
+      username,
+      password
+    );
+    const srpVerifier = await srpClient.deriveVerifier(srpPrivateKey);
+
+    return api.signup({
+      username,
+      srp_salt: toBase64(fromHex(srpSalt)),
+      srp_verifier: toBase64(fromHex(srpVerifier)),
+      private_key_encrypted: toBase64(new Uint8Array(privateKeyEncrypted)),
+      public_key: toBase64(new Uint8Array(publicKey)),
+    });
+  }
+
+  async login(username: string, password: string) {
+    const clientKey = srpClient.generateEphemeral();
+
+    const halloResponse = await api.srpHello({
+      username,
+      srp_client_public_key: toBase64(fromHex(clientKey.public)),
+    });
+
+    const salt = toHex(fromBase64(halloResponse.srp_salt));
+    const serverPublicKey = toHex(
+      fromBase64(halloResponse.srp_server_public_key as string)
+    );
+
+    const privateKey = await srpClient.derivePrivateKey(
+      salt,
+      username,
+      password
+    );
+    const clientSession = await srpClient.deriveSession(
+      clientKey.secret,
+      serverPublicKey,
+      salt,
+      username,
+      privateKey
+    );
+
+    const authResponse = await api.srpAuth({
+      srp_evidence: toBase64(fromHex(clientSession.proof)),
+    });
+
+    await srpClient.verifySession(
+      clientKey.public,
+      clientSession,
+      toHex(fromBase64(authResponse.srp_evidence))
+    );
+
+    return this.createClientSession(password);
+  }
+
+  async createClientSession(password: string | undefined) {
+    const userKeys = await api.getKeys();
+
+    if (password) {
+      const rsaPrivateKeyRawEncrypted = fromBase64(
+        userKeys.private_key_encrypted
+      );
+      const rsaPrivateKeyRaw = await decryptAESGCMPassword(
+        password,
+        rsaPrivateKeyRawEncrypted
+      );
+      const rsaPrivateKey = await importRSAPrivateKey(rsaPrivateKeyRaw);
+
+      await putUserPrivateKey(rsaPrivateKey);
     }
 
-    return response.data;
-  } catch (e: any) {
-    if (axios.isAxiosError(e)) {
-      if (e.response) {
-        throw e.response.data.error;
-      } else {
-        throw { type: "NetworkError", error: e };
+    const privateKey = await getUserPrivateKey();
+
+    if (!privateKey) return null;
+
+    const publicKeyRaw = fromBase64(userKeys.public_key);
+    const publicKey = await importRSAPublicKey(new Uint8Array(publicKeyRaw));
+
+    const userState = await api.getState();
+
+    if (!userState.user) return null;
+
+    return new ClientSession(
+      this.#apiService,
+      userState.user.id,
+      privateKey,
+      publicKey
+    );
+  }
+}
+
+class ClientSession {
+  #cryptoService: CryptoService;
+  #apiService: APIService;
+
+  constructor(
+    apiService: APIService,
+    userid: string,
+    privateKey: CryptoKey,
+    publickey: CryptoKey
+  ) {
+    this.#apiService = apiService;
+    this.#cryptoService = new CryptoService(
+      apiService,
+      userid,
+      privateKey,
+      publickey
+    );
+  }
+
+  async createChatWs(): Promise<ChatWs> {
+    return new Promise((resolve, reject) => {
+      const ws = new WebSocket(`${this.#apiService.baseUrl}/chat/ws`);
+
+      const errorListener = () => {
+        ws.removeEventListener("error", errorListener);
+        reject();
+      };
+
+      ws.addEventListener("error", errorListener);
+
+      ws.addEventListener("open", () => {
+        resolve(new ChatWs(ws, this.#cryptoService));
+      });
+    });
+  }
+}
+
+type DecryptedChatItem = Omit<
+  ChatItem,
+  "receiver_key" | "sender_key" | "cipher"
+> & {
+  message: string;
+};
+
+class CryptoService {
+  #apiService: APIService;
+
+  #currentUserId: string;
+  #currentPrivateKey: CryptoKey;
+  #currentPublicKey: CryptoKey;
+
+  #cachedPublicKey: Map<string, CryptoKey>;
+
+  constructor(
+    apiService: APIService,
+    currentUserId: string,
+    privateKey: CryptoKey,
+    publicKey: CryptoKey
+  ) {
+    this.#apiService = apiService;
+    this.#currentUserId = currentUserId;
+    this.#currentPrivateKey = privateKey;
+    this.#currentPublicKey = publicKey;
+    this.#cachedPublicKey = new Map();
+  }
+
+  getCurrentPrivateKey() {
+    return this.#currentPrivateKey;
+  }
+
+  getCurrentPublicKey() {
+    return this.#currentPublicKey;
+  }
+
+  async getPublicKey(userId: string): Promise<CryptoKey> {
+    if (userId == this.#currentUserId) {
+      return this.#currentPublicKey;
+    }
+
+    if (this.#cachedPublicKey.has(userId)) {
+      return this.#cachedPublicKey.get(userId)!;
+    }
+
+    const raw = typedArrayToBuffer(
+      fromBase64((await this.#apiService.getUser(userId)).public_key)
+    );
+    const key = await importRSAPublicKey(raw);
+
+    this.#cachedPublicKey.set(userId, key);
+
+    return key;
+  }
+
+  async encrypt(userId: string, buffer: BufferSource) {
+    return encryptRSA(await this.getPublicKey(userId), buffer);
+  }
+
+  async getChatData(receiverId: string, message: string) {
+    const messageKey = await generateAESGCMKey();
+    const keyBuffer = await exportRawKey(messageKey);
+    return {
+      receiver_id: receiverId,
+      message_cipher: toBase64(
+        await encryptAESGCM(messageKey, new TextEncoder().encode(message))
+      ),
+      receiver_key: toBase64(
+        new Uint8Array(await this.encrypt(receiverId, keyBuffer))
+      ),
+      sender_key: toBase64(
+        new Uint8Array(await this.encrypt(this.#currentUserId, keyBuffer))
+      ),
+    };
+  }
+
+  async decryptChat({ sender_key, receiver_key, cipher, ...other }: ChatItem) {
+    const encryptedKey = fromBase64(
+      other.sender_id == this.#currentUserId ? sender_key : receiver_key
+    );
+    const keyRaw = await decryptRSA(
+      this.#currentPrivateKey,
+      new Uint8Array(encryptedKey)
+    );
+    const key = await importAESGCMKey(keyRaw);
+    return {
+      ...other,
+      message: new TextDecoder().decode(
+        await decryptAESGCM(key, fromBase64(cipher))
+      ),
+    };
+  }
+}
+
+interface SendChatRequest {
+  receiver_id: string;
+  sender_key: string;
+  receiver_key: string;
+  message_cipher: string;
+}
+
+type WsRequest = { type: "SendChat" } & SendChatRequest;
+type WsReponseMessage = { type: "ReceiveChat" } & ChatItem;
+
+interface ChatWsEventMap {
+  "receive-chat": CustomEvent<DecryptedChatItem>;
+}
+
+class ChatWs extends TypedEventTarget<ChatWsEventMap> {
+  ws: WebSocket;
+  #cryptoservice: CryptoService;
+
+  constructor(ws: WebSocket, cryptoService: CryptoService) {
+    super();
+    this.ws = ws;
+    this.#cryptoservice = cryptoService;
+
+    this.ws.addEventListener("message", this.#onWsMessage.bind(this));
+  }
+
+  sendWsRequest(request: WsRequest) {
+    this.ws.send(JSON.stringify(request));
+  }
+
+  async sendChat(receiverId: string, message: string) {
+    this.sendWsRequest({
+      type: "SendChat",
+      ...(await this.#cryptoservice.getChatData(receiverId, message)),
+    });
+  }
+
+  async #onWsMessage(e: WebSocketEventMap["message"]) {
+    const message = e.data;
+    if (typeof message == "string") {
+      try {
+        const { type, ...rest } = JSON.parse(message) as WsReponseMessage;
+        if (type == "ReceiveChat") {
+          const chat = await this.#cryptoservice.decryptChat(rest);
+          this.dispatchTypedEvent(
+            "receive-chat",
+            new CustomEvent("receive-chat", {
+              detail: chat
+            })
+          );
+        }
+      } catch (e) {
+        console.log(`Server: ${message}`);
       }
-    } else {
-      throw { type: "Unknown", error: e };
     }
   }
 }
 
-const get = (path: string) => runRequest(() => instance.get(path));
-const post = (path: string, data: any) =>
-  runRequest(() => instance.post(path, data));
-
-async function signup(username: string, password: string) {
-  const keyPair = await generateRSAKeyPair();
-  const privateKey = await exportRSAPrivateKey(keyPair.privateKey);
-  const publicKey = await exportRSAPublicKey(keyPair.publicKey);
-
-  const privateKeyEncrypted = await encryptAESGCMPassword(password, privateKey);
-
-  const srpSalt = srpClient.generateSalt();
-  const srpPrivateKey = await srpClient.derivePrivateKey(
-    srpSalt,
-    username,
-    password
-  );
-  const srpVerifier = await srpClient.deriveVerifier(srpPrivateKey);
-
-  return instance.post("/auth/signup", {
-    username,
-    srp_salt: toBase64(fromHex(srpSalt)),
-    srp_verifier: toBase64(fromHex(srpVerifier)),
-    private_key_encrypted: toBase64(new Uint8Array(privateKeyEncrypted)),
-    public_key: toBase64(new Uint8Array(publicKey)),
-  });
-}
-
-async function login(username: string, password: string) {
-  const clientKey = srpClient.generateEphemeral();
-
-  const halloResponse = await post("/auth/hello", {
-    username,
-    srp_client_public_key: toBase64(fromHex(clientKey.public)),
-  });
-
-  const salt = toHex(fromBase64(halloResponse.srp_salt));
-  const serverPublicKey = toHex(
-    fromBase64(halloResponse.srp_server_public_key as string)
-  );
-
-  const privateKey = await srpClient.derivePrivateKey(salt, username, password);
-  const clientSession = await srpClient.deriveSession(
-    clientKey.secret,
-    serverPublicKey,
-    salt,
-    username,
-    privateKey
-  );
-
-  const authResponse = await post("/auth/auth", {
-    srp_evidence: toBase64(fromHex(clientSession.proof)),
-  });
-
-  await srpClient.verifySession(
-    clientKey.public,
-    clientSession,
-    toHex(fromBase64(authResponse.srp_evidence))
-  );
-
-  const rsaPrivateKeyRawEncrypted = fromBase64(
-    (await getKeys()).private_key_encrypted
-  );
-  const rsaPrivateKeyRaw = await decryptAESGCMPassword(
-    password,
-    rsaPrivateKeyRawEncrypted
-  );
-  const rsaPrivateKey = await importRSAPrivateKey(rsaPrivateKeyRaw);
-
-  await putUserPrivateKey(rsaPrivateKey);
-
-  return true;
-}
-
-async function logout() {
-  return post("/auth/logout", null);
-}
-
-async function getState() {
-  return get("/auth/state");
-}
-
-async function getKeys() {
-  return get("/auth/keys");
-}
-
-async function getUser(id: string) {
-  return get(`/user/${id}`);
-}
-
-async function encryptKeyForUser(userid: string, keyBuffer: ArrayBuffer) {
-  const publicKeyRaw = typedArrayToBuffer(
-    fromBase64((await getUser(userid)).public_key)
-  );
-  return encryptRSA(await importRSAPublicKey(publicKeyRaw), keyBuffer);
-}
-
-async function getUserId() {
-  return (await getState()).user.id;
-}
-
-async function getChatData(receiverId: string, message: string) {
-  const messageKey = await generateAESGCMKey();
-  const keyBuffer = await exportRawKey(messageKey);
-  return {
-    receiver_id: receiverId,
-    message_cipher: toBase64(
-      await encryptAESGCM(messageKey, new TextEncoder().encode(message))
-    ),
-    receiver_key: toBase64(
-      new Uint8Array(await encryptKeyForUser(receiverId, keyBuffer))
-    ),
-    sender_key: toBase64(
-      new Uint8Array(await encryptKeyForUser(await getUserId(), keyBuffer))
-    ),
-  };
-}
-
-async function decryptChat(
-  user_id: string,
-  { sender_key, receiver_key, cipher, ...other }: any
-) {
-  const encryptedKey = fromBase64(
-    other.sender_id == user_id ? sender_key : receiver_key
-  );
-  const keyRaw = await decryptRSA(
-    await getUserPrivateKey(),
-    new Uint8Array(encryptedKey)
-  );
-  const key = await importAESGCMKey(keyRaw);
-  return {
-    ...other,
-    message: new TextDecoder().decode(
-      await decryptAESGCM(key, fromBase64(cipher))
-    ),
-  };
-}
-
-async function getChats() {
-  const userPrivateKey = await getUserPrivateKey();
-  const id = (await getState()).user.id;
-  return Promise.all((await get("/chat")).map(decryptChat));
-}
-
-async function runChatWs() {
-   const id = (await getState()).user.id;
-
-  return new Promise((resolve, reject) => {
-    const ws = new WebSocket(`${API_URL}/chat/ws`);
-    const errorListener = () => {
-      ws.removeEventListener("error", errorListener);
-      reject();
-    };
-    ws.addEventListener("error", errorListener);
-    ws.addEventListener("open", () => {
-      const chatListeners: any[] = [];
-
-      ws.addEventListener("message", async (e) => {
-        const message = e.data;
-        if (typeof message == "string") {
-          try {
-            const { type, ...rest } = JSON.parse(message);
-            if (type == "ReceiveChat") {
-              const chat = await decryptChat(id, rest);
-              for (const listener of chatListeners) {
-                listener(chat);
-              }
-            }
-          } catch {
-            console.log(`Server: ${message}`);
-          }
-        }
-      });
-
-      resolve({
-        send: async (receiverId: string, message: string) => {
-          ws.send(
-            JSON.stringify({
-              type: "SendChat",
-              ...(await getChatData(receiverId, message)),
-            })
-          );
-        },
-        addChatListener: (listener: any) => {
-          chatListeners.push(listener);
-        },
-      });
-    });
-  });
-}
-
 async function runChatWsTest() {
-  const ws = await runChatWs();
-  ws.addChatListener((chat) => {
-    console.log(chat.message);
-  })
-  return ws
+  const ws = (await (await client.createClientSession(undefined))?.createChatWs())!;
+  ws.addEventListener("receive-chat", (e) => {
+    console.log(e.detail.message);
+  });
+  return ws;
 }
+
+export const api = new APIService();
+export const client = new ClientService(api);
 
 (globalThis as any).API = {
-  signup,
-  login,
-  logout,
-  getState,
-  getKeys,
-  getUser,
-  getChats,
+  api,
+  client,
   runChatWs: runChatWsTest,
 };
--- client/src/lib/db.ts
+++ client/src/lib/db.ts
@@ -1,18 +1,18 @@
-import Dexie, {} from "dexie"
+import Dexie from "dexie";
 
 const db = new Dexie("app_db");
 
 db.version(1).stores({
-    user_data: "++name, data"
-})
+  user_data: "++name, data",
+});
 
 export async function putUserPrivateKey(key: CryptoKey) {
-    await db.user_data.put({
-        name: "private_key",
-        data: key
-    })
+  await db.user_data.put({
+    name: "private_key",
+    data: key,
+  });
 }
 
-export async function getUserPrivateKey() {
-    return (await db.user_data.get("private_key")).data;
+export async function getUserPrivateKey(): Promise<CryptoKey | undefined> {
+  return (await db.user_data.get("private_key")).data;
 }
