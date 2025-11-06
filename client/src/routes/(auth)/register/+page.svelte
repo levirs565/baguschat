e019f95acc2dd9d404d8c9ae75105b9976338b25
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2025-11-06 20:17:36 +0700

    feat: register

--- client/src/routes/(auth)/register/+page.svelte
+++ client/src/routes/(auth)/register/+page.svelte
@@ -1,65 +1,79 @@
 <script>
-	import { Card, Button, Label, Input, Heading } from 'flowbite-svelte';
-    import { goto } from '$app/navigation';
+  import { Card, Button, Label, Input, Heading } from "flowbite-svelte";
+  import { goto } from "$app/navigation";
+  import { client } from "$lib/api";
 
-	let username = '';
-	let password = '';
-	let confirmPassword = '';
+  let username = "";
+  let password = "";
+  let confirmPassword = "";
 
-	function handleRegister() {
-		if (password !== confirmPassword) {
-			alert('Password tidak cocok!');
-			return;
-		}
-		console.log('Mencoba mendaftar dengan:', { username, password });
-        goto('/login');
-	}
+  function handleRegister() {
+    if (password !== confirmPassword) {
+      alert("Password tidak cocok!");
+      return;
+    }
+    client
+      .signup(username, password)
+      .then(() => {
+        goto("/login");
+      })
+      .catch((e) => alert(e));
+  }
 </script>
 
 <svelte:head>
-	<title>Register</title>
+  <title>Register</title>
 </svelte:head>
 
 <Card class="w-full max-w-md p-6">
-	<form class="flex flex-col space-y-6" on:submit|preventDefault={handleRegister}>
-		<Heading tag="h3" class="text-xl font-medium text-gray-900 dark:text-white">
-			Buat Akun Baru
-		</Heading>
+  <form
+    class="flex flex-col space-y-6"
+    on:submit|preventDefault={handleRegister}
+  >
+    <Heading tag="h3" class="text-xl font-medium text-gray-900 dark:text-white">
+      Buat Akun Baru
+    </Heading>
 
-		<div>
-			<Label for="username" class="mb-2">Username Anda</Label>
-			<Input type="text" id="username" bind:value={username} placeholder="username" required />
-		</div>
+    <div>
+      <Label for="username" class="mb-2">Username Anda</Label>
+      <Input
+        type="text"
+        id="username"
+        bind:value={username}
+        placeholder="username"
+        required
+      />
+    </div>
 
-		<div>
-			<Label for="password" class="mb-2">Password</Label>
-			<Input
-				type="password"
-				id="password"
-				bind:value={password}
-				placeholder="••••••••"
-				required
-			/>
-		</div>
+    <div>
+      <Label for="password" class="mb-2">Password</Label>
+      <Input
+        type="password"
+        id="password"
+        bind:value={password}
+        placeholder="••••••••"
+        required
+      />
+    </div>
 
-		<div>
-			<Label for="confirmPassword" class="mb-2">Konfirmasi Password</Label>
-			<Input
-				type="password"
-				id="confirmPassword"
-				bind:value={confirmPassword}
-				placeholder="••••••••"
-				required
-			/>
-		</div>
+    <div>
+      <Label for="confirmPassword" class="mb-2">Konfirmasi Password</Label>
+      <Input
+        type="password"
+        id="confirmPassword"
+        bind:value={confirmPassword}
+        placeholder="••••••••"
+        required
+      />
+    </div>
 
-		<Button type="submit" class="w-full">Buat Akun</Button>
+    <Button type="submit" class="w-full">Buat Akun</Button>
 
-		<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-			Sudah punya akun?
-			<a href="/login" class="text-blue-700 hover:underline dark:text-blue-500">
-				Login di sini
-			</a>
-		</div>
-	</form>
+    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+      Sudah punya akun?
+      <a href="/login" class="text-blue-700 hover:underline dark:text-blue-500">
+        Login di sini
+      </a>
+    </div>
+  </form>
 </Card>
